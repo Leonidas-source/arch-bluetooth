d07562bc549efc1646d29f87e20a807eb5c338f0
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-05-29 21:00:36 +0700

    Add files via upload

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+red="\e[0;91m"
+bold="\e[1m"
+reset="\e[0m"
+service_check() {
+  systemctl status bluetooth.service | grep "enabled" && third="(service enabled)"
+  systemctl status bluetooth.service | grep "enabled" || third="(service disabled)"
+}
+pkg_check() {
+  pacman -Qi bluez && first="(already installed)"
+  pacman -Qi bluez || first="(not installed)"
+  pacman -Qi pulseaudio-alsa pulseaudio-bluetooth && second="(already installed)"
+  pacman -Qi pulseaudio-alsa pulseaudio-bluetooth || second="(not installed)"
+}
 error() {
   clear
   echo "You must be root to execute this script"
@@ -10,11 +23,13 @@ user_check() {
   [ "$check" == root ] || error
 }
 menu() {
+  pkg_check
+  service_check
   clear
-  echo "
-  1) install bluez
-  2) install pulseaudio-bluetooth
-  3) enable bluetooth
+  echo -e "
+  1) install bluez ${red}${bold}$first${reset}
+  2) install pulseaudio-bluetooth ${red}${bold}$second${reset}
+  3) enable bluetooth ${red}${bold}$third${reset}
   4) exit"
   read -r answr
   [ "$answr" == "1" ] && bluez
